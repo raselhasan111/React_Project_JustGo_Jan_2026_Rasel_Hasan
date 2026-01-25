2d1b3de47fa122ccaf228e36778c367803533a26
Rasel Hasan <raselhasan.cse11@gmail.com>
2026-01-25 12:07:02 +0600

    feat: Set up react-router-dom for navigation and create initial page components.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Categories from './pages/categories'
+import Home from './pages/home'
+import ProductDetail from './pages/product-detail'
+import Products from './pages/products'
+import Search from './pages/search'
+import Settings from './pages/settings'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
-        JustGo Assignment (React + TypeScript + Vite + Tailwind CSS)
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/products/categories" element={<Categories />} />
+        <Route path="/products/search" element={<Search />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
