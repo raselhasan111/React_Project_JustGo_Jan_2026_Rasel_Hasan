5195203dc2d82feefe1a7e203ecaf9794387f2ac
Rasel Hasan <raselhasan.cse11@gmail.com>
2026-01-25 11:22:55 +0600

    chore: initialize React project with Vite, TypeScript, Tailwind CSS, ESLint, and Prettier configurations.

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        JustGo Assignment (React + TypeScript + Vite + Tailwind CSS)
+      </h1>
+    </div>
+  )
+}
+
+export default App
