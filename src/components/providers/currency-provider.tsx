487bc0c0ed098c181ae0dd3a287a1548a6fbdbb6
Rasel Hasan <rasel.hasan@vivasoftltd.com>
2026-01-26 17:32:52 +0600

    style: Adjust indentation

--- src/components/providers/currency-provider.tsx
+++ src/components/providers/currency-provider.tsx
@@ -1,22 +1,22 @@
 import { useState, type ReactNode } from 'react'
 import {
-    CURRENCY_SYMBOLS,
-    CurrencyContext,
-    type Currency,
+  CURRENCY_SYMBOLS,
+  CurrencyContext,
+  type Currency,
 } from '../../context/currency-context'
 
 export function CurrencyProvider({ children }: { children: ReactNode }) {
-    const [currency, setCurrency] = useState<Currency>('USD')
+  const [currency, setCurrency] = useState<Currency>('USD')
 
-    const value = {
-        currency,
-        setCurrency,
-        symbol: CURRENCY_SYMBOLS[currency],
-    }
+  const value = {
+    currency,
+    setCurrency,
+    symbol: CURRENCY_SYMBOLS[currency],
+  }
 
-    return (
-        <CurrencyContext.Provider value={value}>
-            {children}
-        </CurrencyContext.Provider>
-    )
+  return (
+    <CurrencyContext.Provider value={value}>
+      {children}
+    </CurrencyContext.Provider>
+  )
 }
